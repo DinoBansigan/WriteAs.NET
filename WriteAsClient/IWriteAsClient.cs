2399c98101b468c1de1de06f718b7d684ed190e5
Dino Bansigan <39771159+DinoBansigan@users.noreply.github.com>
2023-01-30 22:52:22 -0600

    Added interface for the WriteAsClient class to allow for use with dependency injection.

---
+++ WriteAsClient/IWriteAsClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WriteAs.NET.Client.Models;
+using WriteAs.NET.Shared;
+
+namespace WriteAs.NET
+{
+    public interface IWriteAsClient
+    {
+        void Dispose();
+        Task<List<Post>> GetAllPosts(string alias, SortOrder sortOrder = SortOrder.Descending);
+        Task<Post> GetPostById(string postId);
+        Task<Post> GetPostBySlug(string alias, string slug);
+        Task<List<Post>> GetPostsByPageNumber(string alias, int pageNumber, SortOrder sortOrder = SortOrder.Descending, bool saveToCache = false);
+        Task<List<Post>> Search(string alias, string searchKey, SortOrder sortOrder = SortOrder.Descending);
+    }
+}
